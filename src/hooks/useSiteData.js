c22b8ad0c8a213da2a24e4e0d2b035db39bab16f
brunowinss <josebruno00554@gmail.com>
2026-02-25 19:42:45 -0300

    Update name in DEFAULT_DATA to 'beca barretoo'

--- src/hooks/useSiteData.js
+++ src/hooks/useSiteData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const DEFAULT_DATA = {
-    name: 'beca barreto',
+    name: 'beca barretoo',
     handle: '@becabarreto',
     bio: '🍓 Seja bem-vindo ao meu Privacy, aqui você vai encontrar minhas melhores fotos e vídeos exclusivos',
     stats: {
